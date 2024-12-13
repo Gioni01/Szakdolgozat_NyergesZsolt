227ca208cb21cbbe5d7263314ca5b677273ca9f4
Gioni01 <147532598+Gioni01@users.noreply.github.com>
2024-12-13 16:44:44 +0100

    Add files via upload

---
+++ Microchip Studio/cichica/cichica/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+can.c
+
+lateral.c
+
+longitudinal.c
+
+lcd.c
+
+main.c
+
+peripherals.c
+
+uart.c
+
+ubxprotocol.c
+
